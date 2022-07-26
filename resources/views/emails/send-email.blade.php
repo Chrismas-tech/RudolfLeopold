93d04b765d368387393433c7b67e28b9ca5f5d7f
Chris <lucianichristophe0@gmail.com>
2022-07-26 16:43:54 +0200

    upload files

--- resources/views/emails/send-email.blade.php
+++ resources/views/emails/send-email.blade.php
@@ -7,6 +7,7 @@
     <h3>{{ $mailData['name'] }} ({{$mailData['email']}}) {{$mailData['default_message']}}</h3>
     <p>{{ $mailData['message'] }}</p>
 
+    <h2>Message :</h2>
     <p>{{$mailData['default_end']}}</p>
 </body>
 </html>
