17fa1472325358228f719e25dc7f9f9e4fcd1ded
Chris <lucianichristophe0@gmail.com>
2022-07-26 20:33:24 +0200

    update

--- resources/views/website/pages/page-tracks.blade.php
+++ resources/views/website/pages/page-tracks.blade.php
@@ -59,7 +59,6 @@ CD Album Tracks
 
                     <div class="song-play-area">
                         <div class="song-name">
-                            <p>{{$track->audio_file_name}}</p>
                             <audio preload="auto" controls>
                                 <source src="{{asset($track->audio_file)}}">
                             </audio>
